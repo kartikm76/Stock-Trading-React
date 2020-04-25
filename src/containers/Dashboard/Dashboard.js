c75328cf30d7048f8bcec65ee0b019bb3748746f
kartikm76 <33011675+kartikm76@users.noreply.github.com>
2020-04-24 22:27:53 -0400

    Create Dashboard.js

---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import DashboardControl from '../../components/DashboardControl/DashboardControl';
+import Users from '../Users/Users';
+import StockHolding from '../StockHolding/StockHolding';
+import TradeActivity from '../TradeActivity/TradeActivity';
+import AccountBalance from '../AccountBalance/AccountBalance';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);        
+        this.state = {
+            selectedControl: ''
+        };
+      }
+    
+    dashboardControlSelectionHandler = ( action ) => {
+        this.setState({selectedControl: action});
+    }
+
+    render () {
+        const selectedControl = this.state.selectedControl;
+        let output
+        switch ( selectedControl ) {
+            case 'ALL_USERS':                
+                 output = <Users />;
+                 break;
+            case 'STOCK_HOLDING':
+                output = <StockHolding />;
+                break;
+            case 'TRADE_ACTIVITY':
+                output = <TradeActivity />;
+                break;
+            case 'ACCOUNT_BALANCE':
+                output = <AccountBalance />;
+                break;
+            default:
+                break;
+        }
+
+        return (            
+            <div>                         
+                <DashboardControl label="All Users" clicked={() => this.dashboardControlSelectionHandler( 'ALL_USERS' )} />
+                <DashboardControl label="Stock Holding" clicked={() => this.dashboardControlSelectionHandler( 'STOCK_HOLDING' )}  />
+                <DashboardControl label="Trade Activity" clicked={() => this.dashboardControlSelectionHandler( 'TRADE_ACTIVITY')}  />
+                <DashboardControl label="Account Balance" clicked={() => this.dashboardControlSelectionHandler( 'ACCOUNT_BALANCE')}  />                                
+                {output}
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
